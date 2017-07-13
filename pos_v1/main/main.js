e5ae20d4d5a121891b2ddde892cfa85ae0480400
junjun19971111 <1121968314@qq.com>
2017-07-13 06:10:16 -0400

    Update main.js

--- pos_v1/main/main.js
+++ pos_v1/main/main.js
@@ -1,23 +1,31 @@
 'use strict';
-
-//TODO: 请在该文件中实现练习要求并删除此注释
-'use strict';
 function main() {
-    var Item=[];
+    var itemArry = [];
 
-    Item=getItem(barcodeItem);
+    itemArry = getItem(barcodeItem);
 
-    goodItem=getGoodItem(Item);
+    goodItem = getGoodItem(itemArry);
 
-    Item=getItemPrice(Item);
+    itemArry = getItemPrice(itemArry);
 
-    var allPrice=getAllPrice(Item);
+    var allPrice = getAllPrice(itemArry);
 
-    var allReducePrice=getAllReducePrice(Item);
+    var allReducePrice = getAllReducePrice(itemArry);
 
-    var result="***<没钱赚商店>收据***";
-    for()
-        const barcodeItem = [
+    var result = "***<没钱赚商店>收据***";
+
+    for (var i = 0; i < itemArry.length; i++) {
+        result += '名称：' + itemArry[i].name + '，数量：' + itemArry[i].count + '，单价：' + itemArry[i].price + '(元)，小计：' + itemArry[i].tol_price + '(元)\n';
+    }
+    result += '----------------------\n';
+    result += '总计：' + allPrice + '(元)\n';
+    result += '总计：' + allReducePrice + '(元)\n';
+    result += '**********************';
+
+    console.log(result);
+
+}
+    const barcodeItem = [
     'ITEM000001',
     'ITEM000001',
     'ITEM000001',
@@ -27,55 +35,59 @@ function main() {
     'ITEM000005',
     'ITEM000005-2',
 ];
-function getItem(barcodeArry) {
-    var Item_arry=[];
+function getItem(arryx) {
+    var barcodeArry=arryx;
+    var item_arry=[];
+    var arry=[];
     for(var i=0;i<barcodeArry.length;i++){
-        if(barcodeArry[i].length>barcodeArry[0].length) {
-            barcodeArry[i].split('-');
-            var ware=new Object();
-            ware.barcode=barcodeArry[i][0];
-            ware.count=barcodeArry[i][1];
-            Item_arry.push(ware);
-            barcodeArry.splice(i,1);
+        for(var e=0;e<barcodeArry[i].length;e++) {
+            arry=barcodeArry[i];
+            if (arry[e].indexOf('-')) {
+                var ware=new  Object();
+                ware.count=arry[e+1];
+                arry.slice(e,2);
+                ware.barcode=arry;
+                item_arry.push(ware);
+            }
         }
     }
     for(var j=0;j<barcodeArry.length;j++){
-        for(var k=0;k<Item_arry.length;k++){
-            if(barcodeArry[j]===Item_arry.[k].barcode){
-                Item_arry.[k].count++;
+        for(var k=0;k<item_arry.length;k++){
+            if(barcodeArry[j]===item_arry[k].barcode){
+                item_arry[k].count++;
             }
             else{
-                Item_arry.push({barcode:barcodeArry[j],count:1});
+                item_arry.push({barcode:barcodeArry[j],count:1});
             }
         }
 
     }
-    return Item_arry;
+    return item_arry;
 }      // 计算每个商品的购买数量，并建立一个对象数组
-function getGoodItem(itemArry) {
+function getGoodItem(itemArry1) {
     var allItem=loadAllItems();
     var promotions=loadPromotions();
-    for(var i=0;i<itemArry.length;i++){
-        for(j=0;j<allItem.length;j++) {
-            if (itemArry[i].barcode===allItem[j].barcode){
-                itemArry[i].name=allItem[j].name;
-                itemArry[i].unit=allItem[j].unit;
-                itemArry[i].price=allItem[j].price;
-                itemArry[i].reduce_count=0;
-                itemArry[i].reducePrice=0;
+    for(var i=0;i<itemArry1.length;i++){
+        for(var j=0;j<allItem.length;j++) {
+            if (itemArry1[i].barcode===allItem[j].barcode){
+                itemArry1[i].name=allItem[j].name;
+                itemArry1[i].unit=allItem[j].unit;
+                itemArry1[i].price=allItem[j].price;
+                itemArry1[i].reduce_count=0;
+                itemArry1[i].reducePrice=0;
             }
         }
     }
-    for(var k=0;k<itemArry.length;k++){
-        for(var y=0;y<promotions[barcodes].length;k++){
-            if (itemArry[k]===promotions[barcodes][y]&&itemArry[k].count>2){
-                itemArry[k].count=itemArry.count-1;
-                itemArry[k].reduce_count=1;
+    for(var k=0;k<itemArry1.length;k++){
+        for(var y=0;y<promotions.length;k++){
+            if (itemArry1[k]===promotions[y]&&itemArry1[k].count>2){
+                itemArry1[k].count=itemArry1[k].count-1;
+                itemArry1[k].reduce_count=1;
             }
         }
 
     }
-    return itemArry;
+    return itemArry1;
 }   //计算实际购买的商品数量，和节省数量
 function getItemPrice(arry) {
     var arry1=arry;
@@ -147,13 +159,11 @@ function loadAllItems() {
 }
 function loadPromotions() {
     return [
-        {
-            type: 'BUY_TWO_GET_ONE_FREE',
-            barcodes: [
-                'ITEM000000',
-                'ITEM000001',
-                'ITEM000005'
-            ]
-        }
+        'ITEM000000',
+        'ITEM000001',
+        'ITEM000005'
     ];
+
+
 }
+main();
